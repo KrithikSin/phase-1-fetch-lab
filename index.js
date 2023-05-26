11afd2d24ab90269a1d173fdd34f079d7b3fd194
Krithik Singh <krithiksingh@gmail.com>
2023-05-26 13:08:52 -0400

    fetch lab completed

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    // To pass the tests, don't forget to return your fetch!
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then((resp) => resp.json())
+        .then((json) => renderBooks(json));
 }
+
+// Log 5th book in the series
+// fetch('https://anapioficeandfire.com/api/books')
+//     .then((resp) => resp.json())
+//     .then(function (json) {
+//         for (let i = 0; i < json.length; i++) {
+//             if (i === 5) {
+//                 console.log(json[i].name);
+//             }
+//         }
+//     });
+
+// Total number of pages of all the books
+// let totalPages = 0;
+// fetch('https://anapioficeandfire.com/api/books')
+//     .then((resp) => resp.json())
+//     .then(function (json) {
+//         for (let i = 0; i < json.length; i++) {
+//             totalPages += json[i].numberOfPages;
+//         }
+//         console.log(totalPages);
+//     });
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const main = document.querySelector('main');
+    books.forEach((book) => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', function () {
+    fetchBooks();
 });
